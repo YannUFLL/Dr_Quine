0be79e428f27e836d0f8fc794fc7bddb8a43ac69
YannUFLL <yannlescoul79@free.fr>
2023-09-23 20:58:30 +0200

    first push

---
+++ Colleen.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+//c2
+void printHello(){printf("hello");};
+int main(){//c1
+char*a="#include <stdio.h>%c//c2%cvoid printHello(){printf(%chello%c);};%cint main(){//c1%cchar*a=%c%s%c;printf(a,10,10,34,34,10,10,34,a,34);}";printf(a,10,10,34,34,10,10,34,a,34);}
---
+++ Grace.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <fcntl.h>//comment
+#define PRG int m##a##i##n(){ FILE *file = fopen("Grace_kid.c","a");char*a="#include <stdio.h>%c#include <fcntl.h>//comment%c#define PRG int m##a##i##n(){ FILE *file = fopen(%cGrace_kid.c%c,%ca%c); char*a=%c%s%c;fprintf(file,a,10,10,34,34,34,34,34,a,34,10);}%cPRG";fprintf(file,a,10,10,34,34,34,34,34,a,34,10);}
+PRG
