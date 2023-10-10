d3cc9fcd6c204dbc82c07d554ab45d3a0a40a0ac
YannUFLL <yannlescoul79@free.fr>
2023-10-10 16:51:19 +0200

    I changed the format of the comment in Collen.c

--- C/Collen.c
+++ C/Collen.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-//c2
+/*
+    c2
+*/
 void emptyFunction(){};
-int main(){//c1
+int main(){
+/*
+    c1
+*/
 emptyFunction();
-char*a="#include <stdio.h>%c//c2%cvoid emptyFunction(){};%cint main(){//c1%cemptyFunction();%cchar*a=%c%s%c;printf(a,10,10,10,10,10,34,a,34);}";printf(a,10,10,10,10,10,34,a,34);}
+char*a="#include <stdio.h>%c/*%c    c2%c*/%cvoid emptyFunction(){};%cint main(){%c/*%c    c1%c*/%cemptyFunction();%cchar*a=%c%s%c;printf(a,10,10,10,10,10,10,10,10,10,10,34,a,34);}";printf(a,10,10,10,10,10,10,10,10,10,10,34,a,34);}
