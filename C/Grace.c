bcdda3baf04f00e0d5ba3d130f54b83d1bf9f421
YannUFLL <yannlescoul79@free.fr>
2023-10-10 16:55:29 +0200

    I changed the format of the #defin F(x)int main to comply with the subject rules

--- C/Grace.c
+++ C/Grace.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <fcntl.h>
-#define STR "#include <stdio.h>%c#include <fcntl.h>%c#define STR %c%s%c%c#define FPRINTF fprintf(file,a,10,10,34,a,34,10,10,34,34,10,34,34,10,10,10,10);%c#define FT(x) int main(){ FILE *file = fopen(%cGrace_kid.c%c, x);char*a=STR;FPRINTF;}%cFT(%ca%c)%c/*%c    comment%c*/"
+#define STR "#include <stdio.h>%c#include <fcntl.h>%c#define STR %c%s%c%c#define FPRINTF fprintf(file,a,10,10,34,a,34,10,10,34,34,10,34,34,10,10,10,10);%c#define FT(x)int main(){ FILE *file = fopen(%cGrace_kid.c%c, x);char*a=STR;FPRINTF;}%cFT(%ca%c)%c/*%c    comment%c*/"
 #define FPRINTF fprintf(file,a,10,10,34,a,34,10,10,34,34,10,34,34,10,10,10,10);
-#define FT(x) int main(){ FILE *file = fopen("Grace_kid.c", x);char*a=STR;FPRINTF;}
+#define FT(x)int main(){ FILE *file = fopen("Grace_kid.c", x);char*a=STR;FPRINTF;}
 FT("a")
 /*
     comment
