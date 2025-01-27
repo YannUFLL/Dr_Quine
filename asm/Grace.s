bd3d80dfe17e026432502c9374b08654a9ee55c0
YannUFLL <yannlescoul79@free.fr>
2025-01-27 15:47:54 +0100

    ADDED:
    	- MAIN_INJECTION define
    	- FILE_NAME define
            - extern specificator are now inside the macro
    REMOVED:
    	- FPRINTF define
    	- bits64

--- asm/Grace.s
+++ asm/Grace.s
@@ -1,21 +1,23 @@
-bits 64
 ;comment
-extern fprintf
-extern fopen
 
-%define STR_PGRM "bits 64%1$c;comment%1$cextern fprintf%1$cextern fopen%1$c%1$c%%define STR_PGRM %2$c%3$s%2$c%1$c%%define FRPINTF %1$c%%macro MAIN 1%1$c%1$csection .data%1$c    str_pgrm db STR_PGRM, 0%1$c    file_name db %2$cGrace_kid.asm%2$c, 0%1$c    open_flag db %1, 0%1$c%1$csection .text %1$c    global main%1$c%1$c    main:%1$c        sub rsp, 8%1$c        mov rdi, file_name%1$c        mov rsi, open_flag%1$c        xor rax, rax%1$c        call fopen%1$c        mov rdi, rax%1$c        mov rsi, str_pgrm%1$c        mov rdx, 10%1$c        mov rcx, 34%1$c        mov r8, str_pgrm%1$c        xor rax, rax%1$c        call fprintf%1$c        add rsp, 8%1$c        ret%1$c%%endmacro%1$c%1$cMAIN %2$ca%2$c"
-%define FRPINTF 
-%macro MAIN 1
+%define STR_PGRM ";comment%1$c%1$c%%define STR_PGRM %2$c%3$s%2$c%1$c%%define FILE_NAME %2$cGrace_kid.s%2$c%1$c%%define MAIN_INJECTION main%1$c%%macro PGRM 1%1$c%1$c    extern fprintf%1$c    extern fopen%1$c%1$csection .data%1$c%1$c    str_pgrm db STR_PGRM, 0%1$c    file_name db FILE_NAME, 0%1$c    open_flag db %1, 0%1$c%1$csection .text %1$c    global MAIN_INJECTION%1$c%1$c    MAIN_INJECTION:%1$c        sub rsp, 8%1$c        mov rdi, file_name%1$c        mov rsi, open_flag%1$c        xor rax, rax%1$c        call fopen%1$c        mov rdi, rax%1$c        mov rsi, str_pgrm%1$c        mov rdx, 10%1$c        mov rcx, 34%1$c        mov r8, str_pgrm%1$c        xor rax, rax%1$c        call fprintf%1$c        add rsp, 8%1$c        ret%1$c%%endmacro%1$c%1$cPGRM %2$ca%2$c"
+%define FILE_NAME "Grace_kid.s"
+%define MAIN_INJECTION main
+%macro PGRM 1
+
+    extern fprintf
+    extern fopen
 
 section .data
+
     str_pgrm db STR_PGRM, 0
-    file_name db "Grace_kid.asm", 0
+    file_name db FILE_NAME, 0
     open_flag db %1, 0
 
 section .text 
-    global main
+    global MAIN_INJECTION
 
-    main:
+    MAIN_INJECTION:
         sub rsp, 8
         mov rdi, file_name
         mov rsi, open_flag
@@ -32,4 +34,4 @@ section .text
         ret
 %endmacro
 
-MAIN "a"
+PGRM "a"
